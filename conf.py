9a51653bff13ee0255f52c44376d83efd0a7d5f2
Grant Orchard <gorchard@vmware.com>
2019-06-13 21:11:25 +1000

    Set favicon to Cloud Assembly logo

--- conf.py
+++ conf.py
@@ -104,6 +104,7 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_logo = "_static/socialab-logo.svg"
+html_favicon = '_static/cloudassembly.png'
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
